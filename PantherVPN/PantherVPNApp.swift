7b39df6a1944c117d2abb44655f604bba733b856
Kai <kylepowis@gmail.com>
2025-08-06 12:24:06 +0100

    Initial Commit

---
+++ PantherVPN/PantherVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantherVPNApp.swift
+//  PantherVPN
+//
+//  Created by Kyle Powis on 06/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PantherVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
